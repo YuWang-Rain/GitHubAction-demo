4d796de852ffa7b39e344dd457b9cac0438a9eb0
wangyu.fish <wangyu.fish@bytedance.com>
2022-11-25 14:34:03 +0800

    demo for docker

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 
 
 app.get('/', (req, res) => {
-    res.status(200).send('web app start !');
+    res.status(200).send('web app start !！！！！！');
 })
 
 app.listen(3000, () => console.log('app start at port 3000！'));
